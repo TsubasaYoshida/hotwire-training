b99ea0b05cc6b550e97882a9a4b04dab8811c637
Tsubasa Yoshida <bassar@sonicgarden.jp>
2021-06-05 23:23:25 +0900

    fix: メッセージ投稿を Turbo Streams を使った形に

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,9 @@ class MessagesController < ApplicationController
   def create
     @message = @room.messages.build(message_params)
     if @message.save
-      redirect_to @room
+      render turbo_stream: turbo_stream.prepend(:messages, @message)
+      # 上の書き方でOKだが、明示的には以下のように書く
+      # render turbo_stream: turbo_stream.prepend(:messages, partial: 'messages/message', locals: { message: @message })
     else
       render :new
     end
